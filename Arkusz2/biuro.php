23989eefb205aa9d88113d128661ccfbf53e034c
Lucjoszek <paticab25@gmail.com>
2023-10-24 14:04:18 +0200

    Zdjecia i biuro.php

---
+++ Arkusz2/biuro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turystyka to moja pasja</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header></header>
+    <section class='blok-ciasteczek'></section>
+    <section class='lewy'></section>
+    <section class='prawy'></section>
+
+</body>
+</html>
